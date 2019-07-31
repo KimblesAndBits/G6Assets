8758439fad7a8f47045595ffc8d56bdeaa4ef2db
Vic Caruso <vic.caruso@gmail.com>
2019-07-30 21:18:44 -0700

    admin and accessories html routes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+
+
 var express = require("express");
 
 var db = require("./models");
@@ -9,7 +11,7 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("public/G6-HTML"));
 
 // Routes
 require("./routes/apiRoutes")(app);
