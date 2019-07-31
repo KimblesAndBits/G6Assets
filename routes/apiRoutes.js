e7e51167675aebbfb1628f249d9dc1a4d0675a37
Vic Caruso <vic.caruso@gmail.com>
2019-07-30 19:57:33 -0700

    add asset route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,16 +3,16 @@
 const db = require("../models");
 const Cookies = require("js-cookie");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Register a new User
-  app.get("/api/examples", function(req, res) {
-    db.User.find({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.User.find({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/assign", function(req, res) {
+  app.post("/api/assign", function (req, res) {
     let newAsset = req.body;
     let newAssetAssignment = {};
     switch (newAsset.type) {
@@ -21,7 +21,7 @@ module.exports = function(app) {
           hardware_id: newAsset.assetId,
           user_id: newAsset.userID
         };
-        db.UserHardware.create(newAssetAssignment).then(function(dbExample) {
+        db.UserHardware.create(newAssetAssignment).then(function (dbExample) {
           res.json(dbExample);
         });
         break;
@@ -30,7 +30,7 @@ module.exports = function(app) {
           software_id: newAsset.assetId,
           user_id: newAsset.userID
         };
-        db.UserSoftware.create(newAssetAssignment).then(function(dbExample) {
+        db.UserSoftware.create(newAssetAssignment).then(function (dbExample) {
           res.json(dbExample);
         });
         break;
@@ -39,15 +39,15 @@ module.exports = function(app) {
           accessory_id: newAsset.assetId,
           user_id: newAsset.userID
         };
-        db.UserAccessory.create(newAssetAssignment).then(function(dbExample) {
+        db.UserAccessory.create(newAssetAssignment).then(function (dbExample) {
           res.json(dbExample);
         });
     }
   });
 
   // Delete an example by id
-  app.delete("/api/delete/user/:id", function(req, res) {
-    db.User.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/delete/user/:id", function (req, res) {
+    db.User.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
@@ -57,6 +57,59 @@ module.exports = function(app) {
     db.User.create(newUser).then(dbUser => {
       res.json(dbUser);
     });
+  });
+
+  app.post("/add", (req, res) => {
+    let newAsset = req.body;
+    console.log(req.body);
+
+    // get type of asset being added
+    switch (formInfo.assetType) {
+      case "Hardware":
+        let newHardware = {
+          manufacturer: formInfo.manufacturer,
+          asset_tag: formInfo.asset_tag,
+          serial_number: formInfo.serial_number,
+          model: formInfo.model,
+          asset_type: formInfo.assetType,
+          status: formInfo.status,
+          eol: formInfo.eol,
+          bin_location: formInfo.bin_location,
+          user_id: "1",
+          date_assigned: null
+        };
+        db.Hardware.create(newHardware).then(dbUser => {
+          res.json(dbUser);
+        });
+        break;
+      case "Software":
+        let newSoftware = {
+          manufacturer: formInfo.manufacturer,
+          product: formInfo.product,
+          product_key: formInfo.product_key,
+          expiration_date: formInfo.expiration_date,
+          license_size: formInfo.license_size,
+          license_available: formInfo.license_available
+        };
+        db.Software.create(newSoftware).then(dbUser => {
+          res.json(dbUser);
+        });
+        break;
+      case "Accessory":
+        let newAccessory = {
+          manufacturer: formInfo.manufacturer,
+          model: formInfo.model,
+          accessory_type: formInfo.accessory_type,
+          quantity_available: formInfo.accessory_type,
+          bin_location: formInfo.bin_location
+        };
+        db.Accessory.create(newAccessory).then(dbUser => {
+          res.json(dbUser);
+        });
+        break;
+      default:
+        console.log("No assets added");
+    };
   });
 
   app.post("/login", (req, res) => {
