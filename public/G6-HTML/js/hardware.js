cb75cdd751b6d81ac9bed88364d727297f37d698
KimblesAndBits <wearethekimbles@gmail.com>
2019-08-04 00:42:07 -0700

    just a few small changes to routing

--- public/G6-HTML/js/hardware.js
+++ public/G6-HTML/js/hardware.js
@@ -14,7 +14,9 @@ $.get("/api/all-hardware", function(data) {
       row.append("<td>" + data[i].asset_tag + "</td>");
       row.append("<td>" + data[i].serial_number + "</td>");
       row.append("<td>" + data[i].status + "</td>");
-      row.append("<td><a href='#' class='btn btn-success btn-circle btn-sm float-left' data-toggle='modal' data-target='#assignAsset'><span class='icon text-white-50'><i class='fas fa-check'></i></span></a><a href='#' class='btn btn-warning btn-circle btn-sm float-left' data-toggle='modal' data-target='#assignAsset'><span class='icon text-white-50'><i class='fas fa-exclamation-circle'></i></span></a><a href='#' class='btn btn-danger btn-circle btn-sm float-left' data-toggle='modal' data-target='#assignAsset'><span class='icon text-white-50'><i class='fas fa-trash'></i></span></a></td>");
+      row.append(
+        "<td><a href='#' class='btn btn-success btn-circle btn-sm float-left' data-toggle='modal' data-target='#assignAsset'><span class='icon text-white-50'><i class='fas fa-check'></i></span></a><a href='#' class='btn btn-warning btn-circle btn-sm float-left' data-toggle='modal' data-target='#assignAsset'><span class='icon text-white-50'><i class='fas fa-exclamation-circle'></i></span></a><a href='#' class='btn btn-danger btn-circle btn-sm float-left' data-toggle='modal' data-target='#assignAsset'><span class='icon text-white-50'><i class='fas fa-trash'></i></span></a></td>"
+      );
       row.append("</tr>");
 
       $("#tableRow").prepend(row);
